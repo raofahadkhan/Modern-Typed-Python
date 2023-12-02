e4102f1a77717ed149587bfe8eae6b4010bb6bd3
RAO FAHAD <raofahad046@gmail.com>
2023-12-02 16:04:16 +0500

    Modern python class 1 code

---
+++ 01_class(Hello world String)/class1.py
@@ -0,0 +1,16 @@
+# name:str = 700
+# print(name)
+
+# name1:int = "100"
+# print(name)
+
+# name: list[str] = ['a','b','c']
+# print(name)
+
+# name1 : tuple[str,int,float] = ['fahad', 24, '2.9']
+# print(name)
+# print(type(name))
+
+# name : any = ['fahad', 24, '2.9'] 
+# print(name)
+# print(type(name))
